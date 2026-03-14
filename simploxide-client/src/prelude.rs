f5878394c689572b7760a5466ed36fe8c44d505d
a1akris <alkrivitskiy@pm.me>
2026-03-14 13:59:20 +0200

    simploxide-client: extend prelude

--- simploxide-client/src/prelude.rs
+++ simploxide-client/src/prelude.rs
@@ -1,9 +1,10 @@
 //! Re-exports everything that is needed to send commands, match events and destructure responses
 
 pub use crate::types::{
-    AddressSettings, CIContent, CIDeleteMode, ChatDeleteMode, ChatInfo, ChatRef, ChatType,
-    ComposedMessage, CreatedConnLink, GroupMemberRole, GroupProfile, MsgContent, MsgReaction,
-    NewUser, Preferences, Profile, UpdatedMessage, User, UserInfo,
+    AddressSettings, CIContent, CIDeleteMode, ChatBotCommand, ChatDeleteMode, ChatInfo,
+    ChatPeerType, ChatRef, ChatType, ComposedMessage, CreatedConnLink, CryptoFile, GroupMemberRole,
+    GroupProfile, MsgContent, MsgReaction, NewUser, Preferences, Profile, UpdatedMessage, User,
+    UserInfo,
 };
 pub use crate::{
     ClientApi as _, ClientError, ClientResult, client_api::*, commands::*, events::*, responses::*,
